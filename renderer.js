505c42f25071eff0c55e655e00b31211579525f4
Keir Stephan <kiersteph@gmail.com>
2026-04-12 15:52:00 +0800

    chore: update version to 1.1.3 and fix submit button styles

--- renderer.js
+++ renderer.js
@@ -1,4 +1,4 @@
-const CURRENT_VERSION = "1.0.0";
+const CURRENT_VERSION = "1.1.0";
 
 const VERSION_URL = "https://raw.githubusercontent.com/Keirstephbussiness/Ggsms_System/refs/heads/main/version.json";
 
